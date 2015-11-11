5a4e54ba69f4694f1b2757221498d2e2f92cc2cc
GerryCasey <gerry.casey@hotmail.co.uk>
2015-11-11 14:40:46 +0000

    updated app

--- gapp-master/app/src/main/java/com/example/gapp/DisplayActivity.java
+++ gapp-master/app/src/main/java/com/example/gapp/DisplayActivity.java
@@ -2,16 +2,23 @@ package com.example.gapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.NumberPicker;
 
 /**
  * Created by he282 on 11/11/2015.
  */
 public class DisplayActivity extends AppCompatActivity {
+
+    TextView
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event1);
+
+
     }
+
 
 
 
